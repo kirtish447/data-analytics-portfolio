f0cc143771085fdcddf18f3aa2cca97e18cd8ea3
kirtish447 <kirtishraut35@gmail.com>
2026-03-16 23:03:17 +0530

    Update 06_daily_marketplace_performance.sql

--- marketplace-supply-demand-analytics/queries/06_daily_marketplace_performance.sql
+++ marketplace-supply-demand-analytics/queries/06_daily_marketplace_performance.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    date,
+    SUM(supply_count) AS total_supply,
+    SUM(demand_count) AS total_demand,
+    SUM(completed_trips) AS total_completed_trips,
+    AVG(price) AS avg_price,
+    AVG(surge_multiplier) AS avg_surge
+FROM marketplace_data
+GROUP BY date
+ORDER BY date;
