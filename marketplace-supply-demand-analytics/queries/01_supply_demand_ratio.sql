c4d07627ad163db4b64472fac7525d37f2864234
kirtish447 <kirtishraut35@gmail.com>
2026-03-16 23:01:15 +0530

    Update 01_supply_demand_ratio.sql

--- marketplace-supply-demand-analytics/queries/01_supply_demand_ratio.sql
+++ marketplace-supply-demand-analytics/queries/01_supply_demand_ratio.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    city,
+    AVG(supply_count) AS avg_supply,
+    AVG(demand_count) AS avg_demand,
+    AVG(supply_count * 1.0 / demand_count) AS supply_demand_ratio
+FROM marketplace_data
+GROUP BY city
+ORDER BY supply_demand_ratio;
